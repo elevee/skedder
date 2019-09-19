fb88ba430ffce4993e1aa11779dbaa4f372d4883
Eric Levine <djlevine@gmail.com>
2019-09-18 18:11:22 -0700

    Updated script to parse sportsengine

--- skedder/entry.py
+++ skedder/entry.py
@@ -6,6 +6,7 @@ from contextlib import closing
 from bs4 import BeautifulSoup
 # from gcal import main
 import utilities as utils
+import time
 from datetime import datetime, timedelta
 import dateutil.parser as parser
 from pprint import pprint
@@ -14,58 +15,127 @@ import sys
 
 calendars = ConfigLoader().config() #load in team calendars
 
-def parse_games(url):
-    # print(calendar['url'])
-    
+def parse_games(teamName, url):
     def column_is(i):
         return {
             0: "date",
-            1: "time",
-            2: "home",
-            3: "away",
-            4: "location"
+            1: "result",
+            2: "opponent",
+            3: "location",
+            4: "time"
         }.get(i, "invalid")    # invalid is default if i not found
 
     source = requests.get(url).text
     soup = BeautifulSoup(source, 'html.parser')
-    rows = soup.find('div', class_='tupcoming').find_all('tr', class_='hidden-xs') # Site already filters games that have been played
+    rows = soup.find('tbody').find_all('tr') # Site already filters games that have been played
     games = []
     game_count = 0
-    print('row length is', len(rows))
+    first_game_month = None #need to track first game month to compare when calculating year
+    print('you have ' + str(len(rows)) + ' total games:')
     for row in rows:
         game = {}
+        if 'completed' in row['class']:
+            print('this is a completed game')
+            game['completed'] = True;
+        else:
+            print('this is a scheduled game')
+            game['completed'] = False;
         colIdx = 0
         for cell in row.find_all('td'):
             column = column_is(colIdx)
+            # print('column is '+column)
             output = ""
             if column == "date":
-                date_array = cell.text.strip('\n').split(' ')
-                date_array.pop(0) #remove day string
+                date_array = utils.cell_data_to_list(cell)
+                date_array.pop(0) #remove day string (e.g. Mon)
+
+                if rows.index(row) == 0: #first game of season
+                    first_game_month = utils.getMonthNumber(date_array[0]) #set first game month
+                
+                date_array.append(str(
+                    utils.getGameYear(
+                        first_game_month,
+                        utils.getMonthNumber(date_array[0])
+                    )
+                ))
                 _date = ' '.join(date_array)
-                output = datetime.strptime(_date, '%b %d, %Y').strftime('%m-%d-%Y')
+                output = utils.format(column, _date)
+            elif column == "result":
+                output = None
+            elif column == "opponent":
+                opp_array = utils.cell_data_to_list(cell)
+                is_home = False if len(opp_array) > 1 and opp_array[0].strip() == "@" else True
+                game['isHome'] = is_home
+                if not is_home:
+                    opp_array.pop(0)
+                output = ' '.join(opp_array)
+                game['away'] = output if is_home else teamName
+                game['home'] = teamName if is_home else output
             elif column == "time":
-                output = cell.text.split('-')[0].strip()
+                if game['completed']:
+                    print('game\'s been completed')
+                    game['boxscore_url'] = cell.a['href']
+                    output = None
+                else:
+                    output = utils.format(column, cell)
             else:
                 output = cell.text.strip()
-            game[column] = output 
+            game[column] = output
             colIdx = colIdx+1
-            # print('++++')
-        # print(f"Game #{game_count+1}", game)
         games.append(game)
         game_count = game_count+1
         # print('____________')
+    pprint(games)
     return games
 
+def parse_boxscore(game):
+    boxscore = {}
+    html = []
+    source = requests.get(game['boxscore_url']).text
+    soup = BeautifulSoup(source, 'html.parser')
+    
+    gamesheet_url = soup.find('div', class_='live_game_sheet').find('a')['href']
+    game_sheet_source = requests.get(gamesheet_url).text
+    gamesheet_soup = BeautifulSoup(game_sheet_source, 'html.parser')
+    boxscore['time'] = utils.format('time', gamesheet_soup.find('span', {'id': 'game_time'}))
+    scores = soup.find('span', class_='scores')
+    scoring_summary = soup.find('ul', class_='scoring_summary')
+    html.append(f"<h1>{game['away']}  {scores.find('span', class_='away').text}   ")
+    html.append(f"{game['home']}  {scores.find('span', class_='home').text} </h1>")
+    html.append(f"More: <a href='{game['boxscore_url']}'>Gamesheet</a><br><br>")
+    
+    # for li in scoring_summary.find_all('li'):
+    #     if li['class'] and 'interval_row' in li['class']:
+    #         # print(li.find('span').text.strip())
+    #         print('hi')
+    #         # html.append(f"<h4>{li.find('span').text} Period</h4>")
+    #     if 'scoring_info' in li['class']:
+    #         print('score!')
+    
+    boxscore['description'] = ''.join(html)
+
+    #we return the time (which is no longer available on the sked), and the description, a long string with final score and link to gamesheet
+    return boxscore 
 
 def lambda_handler(event, context):
     if utils.service:
         for calendar in calendars:
-            games = parse_games(calendar['url'])
+            games = parse_games(calendar['team'], calendar['url'])
             if games and len(games) > 0:
                 pprint(games)
+
                 for game in games: # For each (remaining) game:
-                    # print(f"{game['away']} at {game['home']} already on cal? ", "Yes" if game_already_on_date(game, calendar['gcal_id']) else "No") # check calendar for existing event for that team on that day
-                    
+                    game_string = (game['opponent'] if game['isHome'] else calendar['team']) + \
+                        " at " + \
+                        (calendar['team'] if game['isHome'] else game['opponent'])
+                    # print(game_string + " already on cal? " + ("Yes" if utils.game_already_on_date(game, calendar['gcal_id']) else "No")) # check calendar for existing event for that team on that day
+                    description = None
+
+                    if 'completed' in game and 'boxscore_url' in game:
+                        boxscore_obj = parse_boxscore(game)
+                        game['time'] = boxscore_obj['time'] # start time brought back from the dead!
+                        game['description'] = boxscore_obj['description']
+
                     existing_game = utils.game_already_on_date(game, calendar['gcal_id'])
                 
                     #cleanup events
@@ -78,14 +148,17 @@ def lambda_handler(event, context):
 
                     if existing_game: # if one exists, the datetime, location, home, away teams match up, continue the loop (skipping an insert)
                         if (utils.is_same_game(existing_game, game)):
-                            print(f"{game['away']} at {game['home']} game exists and is correct; not continuing work.")
+                            if 'completed' in game and game['completed'] and not 'description' in existing_game:
+                                utils.update_game(game, calendar['gcal_id'], existing_game) # add completed game (update description)
+                                print(f"{game_string} game exists but has been updated with scoresheet info")
+                            else:
+                                print(f"{game_string} game exists and is correct; not continuing work.")
                         else:
-                            print(f"{game['away']} at {game['home']}: something's different. Deleting existing one.")
-                            # if remove_game(existing_game, calendar['gcal_id']): # else, delete the existing event for that day for that team
-                            #     add_game(game, calendar['gcal_id'])
+                            print(f"{game_string}: something's different. Deleting existing one.")
+                            utils.add_game(game, calendar['gcal_id']) # add new game (old one is set to delete at end of func)
                     else:
                         utils.add_game(game, calendar['gcal_id'])
-                # cleanup any orphaned events on different dates
+                # cleanup any orphaned events on different dates or with wrong info
                 calEvents = utils.games_on_calendar(games[0]['date'], calendar['gcal_id']) #grab events from date of first remaining game
                 orphaned_games = utils.find_outdated_events(calEvents, games)
                 if orphaned_games and len(orphaned_games) > 0:
@@ -95,7 +168,7 @@ def lambda_handler(event, context):
                         print('Orphaned record about to be removed: ', orphan['id']) # remove games from calEvents as we check their validity
                         utils.remove_game(orphan, calendar['gcal_id'])
             else:
-                print("No new games for ", calendar['team'])
+                print("No games found for ", calendar['team'])
 
 
 # sys.exit('exiting early')
--- skedder/utilities.py
+++ skedder/utilities.py
@@ -1,4 +1,5 @@
 from gcal import main
+from time import strptime
 from datetime import datetime, timedelta
 import dateutil.parser as parser
 from pprint import pprint
@@ -6,35 +7,65 @@ import arrow #for DST
 
 import sys
 
+# FYI THERE ARE TWO strptime METHODS. One on datetime, one on time
+
 #load gcal in and make it available
 service = main({
-    "production": True
-    # "testing": True
+    # "production": True
+    "testing": True
 })
 
-def rfcify(date, time=None): #converts dates from '%m-%d-%Y' [HH:MM:SS] to rfc format
-    dst = arrow.get(date, "MM-DD-YYYY").to('US/Pacific').dst()
-    offset = "-07:00" if dst.seconds > 0 else "-08:00"
-    if time: # date & time
-        return parser.parse(f"{date} {time}").isoformat() + offset
-    else: # date only
-        return datetime.strptime(date, '%m-%d-%Y').isoformat() + offset
+def add_game(game, calId):
+    pprint('game is')
+    pprint(game)
+    start_time = rfcify(game['date'], game['time'])
+    tz = 'America/Los_Angeles'
+    event = {
+        'summary': matchup_format(game['away'], game['home']),
+        'location': game['location'],
+        'start': {
+            'dateTime': start_time,
+            'timeZone': tz,
+        },
+        'end': {
+            'dateTime': add_hours(start_time),
+            'timeZone': tz,
+        }
+    }
+    if 'description' in game:
+        event['description'] = game['description']
+    print(f"Adding {event['summary']}")
+    try:
+        return service.events().insert(calendarId=calId, body=event).execute()
+    except:
+        print('Error removing game', game['id'], matchup_format(game['away'], game['home']))
+        return False
 
 def add_hours(start, hours=2):
     return (parser.parse(start) + timedelta(hours=hours)).isoformat()
 
-def location_case(location):
-    return location if location == "NHL" else location.title()
+def cell_data_to_list(raw_cell):
+    array_with_empty_items = raw_cell.text.strip().split(' ')
+    print(array_with_empty_items)
+    return list(filter(None, array_with_empty_items)) #gets rid of empty array elements
 
-def is_same_game(calEvent, webEvent):
-    return (rfcify(webEvent['date'], webEvent['time']) == calEvent['start']['dateTime'] and 
-        matchup_format(webEvent['away'], webEvent['home']) == calEvent['summary'] and
-        location_case(webEvent['location']) == calEvent['location'])
+def find_outdated_events(calEvents, webEvents):
+    # print("ooh we removing outdated!")
+    remaining_events = calEvents
+    for webEvent in webEvents: #for each event on the official cal
+        for calEvent in remaining_events: # check whether existing gcal events
+            if is_same_game(calEvent, webEvent): # are the same
+                remaining_events.remove(calEvent) # and exclude them from being deleted
+                continue
+    return remaining_events
 
-def is_inside_games(calEvent, games):
-    # todo: implement as a lambda function to find out if a calEvent is inside games array at all. Could delete right then.
-    # return list(filter(lambda x: x['id'] === xxxx, calEvent))
-    pass
+def format(input_type, cell):
+    if input_type == 'date':
+        return datetime.strptime(cell, '%b %d %Y').strftime('%m-%d-%Y')
+    elif input_type == 'time':
+        time_array = cell.text.strip().split(' ')
+        time_array.pop() # get rid of PST/PDT timezone
+        return ' '.join(time_array)
 
 def game_already_on_date(game, calId):
     # print("game date? ", game['date'])
@@ -54,30 +85,25 @@ def games_on_calendar(startDate, calId): # provided a start date, returns list o
     except Exception as exception:
         print("something went wrong with games_on_calendar", exception)
 
+def getMonthNumber(monthAbbrev):
+    return strptime(monthAbbrev, '%b').tm_mon
+
+def getGameYear(initialGameMonth, currentGameMonth):
+    current_year = datetime.now().year;
+    return current_year + 1 if initialGameMonth > currentGameMonth else current_year
+
+def is_same_game(calEvent, webEvent):
+    return (rfcify(webEvent['date'], webEvent['time']) == calEvent['start']['dateTime'] and 
+        matchup_format(webEvent['away'], webEvent['home']) == calEvent['summary'] and
+        webEvent['location'] == calEvent['location'])
+
+def is_inside_games(calEvent, games):
+    # todo: implement as a lambda function to find out if a calEvent is inside games array at all. Could delete right then.
+    # return list(filter(lambda x: x['id'] === xxxx, calEvent))
+    pass
+
 def matchup_format(away, home):
     return f"{away} @ {home}"
-
-def add_game(game, calId):
-    start_time = rfcify(game['date'], game['time'])
-    tz = 'America/Los_Angeles'
-    event = {
-        'summary': matchup_format(game['away'], game['home']),
-        'location': location_case(game['location']),
-        'start': {
-            'dateTime': start_time,
-            'timeZone': tz,
-        },
-        'end': {
-            'dateTime': add_hours(start_time),
-            'timeZone': tz,
-        }
-    }
-    print(f"Adding {event['summary']}")
-    try:
-        return service.events().insert(calendarId=calId, body=event).execute()
-    except:
-        print('Error removing game', game['id'], matchup_format(game['away'], game['home']))
-        return False
 
 def remove_game(game, calId):
     try:
@@ -87,12 +113,20 @@ def remove_game(game, calId):
         print('Error removing game', game['id'], matchup_format(game['away'], game['home']))
         return False
 
-def find_outdated_events(calEvents, webEvents):
-    # print("ooh we removing outdated!")
-    remaining_events = calEvents
-    for webEvent in webEvents: #for each event on the official cal
-        for calEvent in remaining_events: # check whether existing gcal events
-            if is_same_game(calEvent, webEvent): # are the same
-                remaining_events.remove(calEvent) # and exclude them from being deleted
-                continue
-    return remaining_events
+def rfcify(date, time=None): #converts dates from '%m-%d-%Y' [HH:MM:SS] to rfc format
+    dst = arrow.get(date, "MM-DD-YYYY").to('US/Pacific').dst()
+    offset = "-07:00" if dst.seconds > 0 else "-08:00"
+    if time: # date & time
+        return parser.parse(f"{date} {time}").isoformat() + offset
+    else: # date only
+        return datetime.strptime(date, '%m-%d-%Y').isoformat() + offset
+
+def update_game(game, calId, existing_game):
+    event = existing_game
+    event['description'] = game['description'] #just adding description to the existing event
+    print(f"Updating Event ID:{existing_game['id']}")
+    try:
+        return service.events().update(calendarId=calId, eventId=existing_game['id'], body=event).execute()
+    except:
+        print('Error updating game', existing_game['id'])
+        return False
